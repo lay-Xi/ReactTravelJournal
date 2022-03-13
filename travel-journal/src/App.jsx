e6e4d06cae90b968c4252432a6dc6a8eba07d8e2
Xi Gao <xiga6451@colorado.edu>
2022-03-13 15:07:44 -0600

    Added components and fa icons

---
+++ travel-journal/src/App.jsx
@@ -0,0 +1,11 @@
+import Navbar from "./components/Navbar";
+import Location from "./components/Locations";
+
+export default function App() {
+  return (
+    <div>
+      <Navbar />
+      <Location />
+    </div>
+  );
+}
