1646862ab05dadfcd82d866274f099a62611cb8d
Xi Gao <xiga6451@colorado.edu>
2022-03-13 15:21:34 -0600

    Added nav bar

--- travel-journal/src/components/Locations.jsx
+++ travel-journal/src/components/Locations.jsx
@@ -1,7 +1,6 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { solid } from '@fortawesome/fontawesome-svg-core/import.macro';
+
 export default function Location() {
-    return (
-        <div>
-            Location comp
-        </div>
-    )
+  return <div>Location comp</div>;
 }
--- travel-journal/src/components/Navbar.jsx
+++ travel-journal/src/components/Navbar.jsx
@@ -1,11 +1,11 @@
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { solid } from '@fortawesome/fontawesome-svg-core/import.macro'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { solid } from '@fortawesome/fontawesome-svg-core/import.macro';
 
 export default function Navbar() {
-    return (
-        <nav>
-            <FontAwesomeIcon icon={solid('location-dot')} />
-            Nav Comp
-        </nav>
-    )
+  return (
+    <nav>
+      <FontAwesomeIcon icon={solid('globe')} />
+      &nbsp; my travel journal.
+    </nav>
+  );
 }
