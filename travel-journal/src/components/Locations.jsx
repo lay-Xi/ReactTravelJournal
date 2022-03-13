3657e5afc28ce296209b67df8db4417e861c60a5
Xi Gao <xiga6451@colorado.edu>
2022-03-13 15:31:57 -0600

    Created location comp details

--- travel-journal/src/components/Locations.jsx
+++ travel-journal/src/components/Locations.jsx
@@ -2,5 +2,19 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { solid } from '@fortawesome/fontawesome-svg-core/import.macro';
 
 export default function Location() {
-  return <div>Location comp</div>;
+  return (
+    <div>
+      <img src='https://source.unsplash.com/WLxQvbMyfas' alt='' />
+      <span>
+        <FontAwesomeIcon icon={solid('location-dot')} />
+        <p>JAPAN</p>
+        <a href='https://goo.gl/maps/1DGM5WrWnATgkSNB8'>View on Google Maps</a>
+      </span>
+      <h1>Mount Fuji</h1>
+      <span>
+        <b>start - end</b>
+      </span>
+      <p>description</p>
+    </div>
+  );
 }
